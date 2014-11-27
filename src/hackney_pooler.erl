c371f8d5907d4cbb23446e4fd8ed1b068c7c5901
benoitc <bchesneau@gmail.com>
2014-11-27 15:39:15 +0100

    better messages error

--- src/hackney_pooler.erl
+++ src/hackney_pooler.erl
@@ -225,8 +225,8 @@ do_async_request(PoolName, To, HPool,  Method, Url, Headers, Body, Options) ->
         send_async(To, PoolName, Reply)
     catch
         _:_ ->
-            error_logger:error("** hackney_pooler ~p: unexpected error"
-                                "(ignored): ~w~n", [PoolName,
+            error_logger:error("** hackney_pooler (async req) ~p: "
+                                "unexpected error (ignored): ~w~n", [PoolName,
                                                     erlang:get_stacktrace()])
     end,
     ok.
